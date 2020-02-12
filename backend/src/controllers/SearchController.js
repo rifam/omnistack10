34876b06b33828d84b01f173dfee36e0b8bf6f2b
rifam <vinicius.rifam@gmail.com>
2020-02-12 11:11:34 -0300

    Mecanismo de busca

--- backend/src/controllers/SearchController.js
+++ backend/src/controllers/SearchController.js
@@ -1,1 +1,27 @@
-const Dev = require
+const Dev = require("../models/Dev");
+const parseStringAsArray = require("../utils/parseStringAsArray");
+
+module.exports = {
+  async index(request, response) {
+    const { latidude, longitude, techs } = request.query;
+
+    const techsArray = parseStringAsArray(techs);
+
+    const devs = await Dev.find({
+      techs: {
+        $in: techsArray
+      },
+      location: {
+        $near: {
+          $geometry: {
+            type: "Point",
+            coordinates: [longitude, latidude]
+          },
+          $maxDistance: 10000,
+        }
+      }
+    });
+
+    return response.json({ devs });
+  }
+};
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,13 @@
 const { Router } = require('express');
 const DevController = require('./controllers/DevController');
+const SearchController = require ('./controllers/SearchController');
 
 
 const routes = Router();
 
 routes.get('/devs', DevController.index);
 routes.post('/devs', DevController.store);
+
+routes.post('/devs', SearchController.index);
 
 module.exports = routes;
