79cfeb4f18be8a0ac0ba55fd5dcf90f42bffb74e
rifam <vinicius.rifam@gmail.com>
2020-02-14 16:29:08 -0300

    Componentização e ajustes layout

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,20 @@
-const express = require ('express');
-const mongoose = require ('mongoose');
-const routes = require ('./routes')
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const routes = require("./routes");
 
 const app = express();
 
-mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-5tbah.mongodb.net/week10?retryWrites=true&w=majority',{
+mongoose.connect(
+  "mongodb+srv://omnistack:omnistack@cluster0-5tbah.mongodb.net/week10?retryWrites=true&w=majority",
+  {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+    useUnifiedTopology: true
+  }
+);
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-
-
-
 
 app.listen(3333);
--- web/src/App.js
+++ web/src/App.js
@@ -1,110 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
+
 import "./global.css";
 import "./App.css";
 import "./Sidebar.css";
 import "./Main.css";
+
+import DevItem from "./components/DevItem";
+import DevForm from "./components/DevForm";
+
 function App() {
+  const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get("/devs");
+
+      setDevs(response.data);
+    }
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(data) {
+    const response = await api.post("/devs", data);
+
+    setDevs([...devs, response.data]);
+  }
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required />
-          </div>
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required />
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required />
-            </div>
-
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required />
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars2.githubusercontent.com/u/31110945?s=460&v=4"
-                alt="rifam"
-              />
-              <div className="user-info">
-                <strong>Vinicius Rifam</strong>
-                <span>HTML, CSS, JavaScript, ReactJS</span>
-              </div>
-            </header>
-            <p>Loreipsum tops radioactive 1000 grau</p>
-            <a href="https://github.com/rifam">Acessar perfil no Github</a>  
-          </li>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars2.githubusercontent.com/u/31110945?s=460&v=4"
-                alt="rifam"
-              />
-              <div className="user-info">
-                <strong>Vinicius Rifam</strong>
-                <span>HTML, CSS, JavaScript, ReactJS</span>
-              </div>
-            </header>
-            <p>Loreipsum tops radioactive 1000 grau</p>
-            <a href="https://github.com/rifam">Acessar perfil no Github</a>  
-          </li>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars2.githubusercontent.com/u/31110945?s=460&v=4"
-                alt="rifam"
-              />
-              <div className="user-info">
-                <strong>Vinicius Rifam</strong>
-                <span>HTML, CSS, JavaScript, ReactJS</span>
-              </div>
-            </header>
-            <p>Loreipsum tops radioactive 1000 grau</p>
-            <a href="https://github.com/rifam">Acessar perfil no Github</a>  
-          </li>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars2.githubusercontent.com/u/31110945?s=460&v=4"
-                alt="rifam"
-              />
-              <div className="user-info">
-                <strong>Vinicius Rifam</strong>
-                <span>HTML, CSS, JavaScript, ReactJS</span>
-              </div>
-            </header>
-            <p>Loreipsum tops radioactive 1000 grau</p>
-            <a href="https://github.com/rifam">Acessar perfil no Github</a>  
-          </li>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars2.githubusercontent.com/u/31110945?s=460&v=4"
-                alt="rifam"
-              />
-              <div className="user-info">
-                <strong>Vinicius Rifam</strong>
-                <span>HTML, CSS, JavaScript, ReactJS</span>
-              </div>
-            </header>
-            <p>Loreipsum tops radioactive 1000 grau</p>
-            <a href="https://github.com/rifam">Acessar perfil no Github</a>  
-          </li>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
         </ul>
       </main>
     </div>
